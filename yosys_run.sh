b69bf84b1d944ade9085a3d4e6079556704297dd
sanampudig <110079648+sanampudig@users.noreply.github.com>
2022-07-28 10:21:51 +0530

    Create yosys_run.sh

---
+++ yosys_run.sh
@@ -0,0 +1,14 @@
+# read design
+
+read_verilog iiitb_pwm_gen.v
+
+# generic synthesis
+synth -top iiitb_pwm_gen
+
+# mapping to mycells.lib
+dfflibmap -liberty sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty sky130_fd_sc_hd__tt_025C_1v80.lib
+clean
+flatten
+# write synthesized design
+write_verilog -assert iiitb_pwm_gen_synth.v
