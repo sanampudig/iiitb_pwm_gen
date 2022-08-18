125b927c4111fed9033cf6da4428e0e1fa7d5369
Gopala Krishna Reddy <110079648+sanampudig@users.noreply.github.com>
2022-08-18 08:13:21 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -1,15 +1,17 @@
 # read design
-
+# read_liberty -lib /media/psf/Home/Stark/Education/ASIC/iiitb_pwm_gen/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 read_verilog iiitb_pwm_gen.v
 
 # generic synthesis
 synth -top iiitb_pwm_gen
 
 # mapping to mycells.lib
-dfflibmap -liberty /home/svgkr7/Desktop/iiitb_pwm_gen/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-abc -liberty /home/svgkr7/Desktop/iiitb_pwm_gen/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
+
+dfflibmap -liberty /media/psf/Home/Stark/Education/ASIC/iiitb_pwm_gen/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /media/psf/Home/Stark/Education/ASIC/iiitb_pwm_gen/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
 clean
 flatten
 # write synthesized design
 write_verilog -noattr iiitb_pwm_gen_synth.v
-
+# stat
+# show
